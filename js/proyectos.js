15e5ffb0705a9c40ad9b2c58382db0db421776ab
lamelaa <160509618+lamelaa@users.noreply.github.com>
2025-04-18 17:39:32 -0500

    Update proyectos.js

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,5 +1,6 @@
+// Inicializa la librería AOS (Animate On Scroll)
 AOS.init({
-    duration: 800, // Duración en ms
-    once: true, // Animar solo una vez
-    easing: 'ease-in-out',
+    duration: 800, // Duración de la animación en milisegundos (800ms)
+    once: true, // La animación se ejecutará solo una vez, no se repetirá
+    easing: 'ease-in-out', // Efecto de aceleración de la animación, pasando de lento a rápido y luego a lento
 });
