5da8bbb5efe48f318e2641645e023cd72280beb8
lamelaa <160509618+lamelaa@users.noreply.github.com>
2025-04-22 15:59:58 -0500

    Update proyectos.js

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,5 +1,5 @@
 AOS.init({
-    duration: 800, // Duración en ms
-    once: true, // Animar solo una vez
-    easing: 'ease-in-out',
+    duration: 800, // Duración en milisegundos para la animación
+    once: true, // La animación solo se ejecutará una vez cuando el elemento entre en el área visible
+    easing: 'ease-in-out', // Tipo de suavizado para la animación, proporcionando un inicio y un final suaves
 });
